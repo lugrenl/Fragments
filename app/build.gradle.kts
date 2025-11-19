d3374b4c334cecab3234704cda37a4fd035eb059
lugrenl <lugrenl@gmail.com>
2025-11-19 17:51:13 +0300

    add_dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,18 +36,19 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    buildFeatures {
-        compose = true
-    }
 }
 
 dependencies {
+    implementation("androidx.fragment:fragment:1.8.5")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.appcompat)
     implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
@@ -58,3 +59,4 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
+
