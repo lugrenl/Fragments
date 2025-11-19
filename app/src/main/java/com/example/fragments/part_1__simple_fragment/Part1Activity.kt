4cf68c7a18063c3d194cfcd17b9b0bbe76ce7f9d
lugrenl <lugrenl@gmail.com>
2025-11-19 17:53:02 +0300

    part_1

--- app/src/main/java/com/example/fragments/part_1__simple_fragment/Part1Activity.kt
+++ app/src/main/java/com/example/fragments/part_1__simple_fragment/Part1Activity.kt
@@ -1,10 +1,32 @@
 package com.example.fragments.part_1__simple_fragment
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fragments.R
 
-class Part1Activity : ComponentActivity() {
+
+/**
+ * Part 1 -- Hello, fragments
+ *
+ * Первое знакомство с фрагментами.
+ *
+ * - Создаём фрагмент
+ * - Проверяем зависимости от нужных библиотек
+ * - Вёрстка фрагментов
+ * - Правильный метод жизненного цикла и конструктор
+ * - Добавление фрагмента в вёрстку (<fragment/> vs <FragmentContainerView/>)
+ * - Добавление фрагмента из кода
+ */
+
+class Part1Activity : AppCompatActivity(R.layout.activity_part_1) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        supportFragmentManager.beginTransaction()
+            .add(
+                R.id.activity_part1_fragment_container_view,
+                Part1Fragment()
+            )
+            .commit()
     }
 }
---
+++ app/src/main/java/com/example/fragments/part_1__simple_fragment/Part1Fragment.kt
@@ -0,0 +1,9 @@
+package com.example.fragments.part_1__simple_fragment
+
+import com.example.fragments.R
+import androidx.fragment.app.Fragment
+
+
+internal class Part1Fragment : Fragment(R.layout.fragment_part1){
+
+}
