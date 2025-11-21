0dcf25d23cc6457f4c4cf4426c987b2f1bf80081
lugrenl <lugrenl@gmail.com>
2025-11-21 18:01:32 +0300

    part_4

--- app/src/main/java/com/example/fragments/part_4__lifecycle/Part4Activity.kt
+++ app/src/main/java/com/example/fragments/part_4__lifecycle/Part4Activity.kt
@@ -1,10 +1,31 @@
 package com.example.fragments.part_4__lifecycle
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fragments.R
+import com.example.fragments.part_1__simple_fragment.Part1Fragment
 
-class Part4Activity : ComponentActivity() {
+
+/**
+ * Part 4 -- Fragment`s lifecycle
+ *
+ * - LifecycleExampleFragment - просто фрагмент с логами
+ * - Добавляем вёрстку EditText, кейс с переворотом экрана
+ * - Don`t keep асtivities - смотрим на логи
+ * - Переходы на следующий fragment (без анимации)
+ * - Переход на следующий фрагмент (с анимацией)
+ *
+ * - Lifecycle-aware компонент
+ */
+
+class Part4Activity : AppCompatActivity(R.layout.activity_part_4) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.activity_part_4_container, Part4Fragment.newInstance("first"))
+                .commit()
+        }
     }
 }
---
+++ app/src/main/java/com/example/fragments/part_4__lifecycle/Part4Fragment.kt
@@ -0,0 +1,97 @@
+package com.example.fragments.part_4__lifecycle
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import com.example.fragments.R
+
+
+internal class Part4Fragment : Fragment(R.layout.fragment_part4){
+
+    companion object {
+        private const val ARGS_TITLE = "args_title"
+        private const val LOG_TAG = "LifecycleExample"
+
+        fun newInstance(title: String) : Fragment {
+            return Part4Fragment().also {
+                it.arguments = bundleOf(ARGS_TITLE to title)
+            }
+        }
+    }
+
+    private val title: String
+        get(){
+            return arguments?.getString(ARGS_TITLE) ?: ""
+        }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(LOG_TAG, "$title - onAttach")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(LOG_TAG, "$title - onCreate")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState).also {
+            Log.d(LOG_TAG, "$title - onCreateView")
+        }
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(LOG_TAG, "$title - onViewCreated")
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        Log.d(LOG_TAG, "$title - onViewStateRestored")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(LOG_TAG, "$title - onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(LOG_TAG, "$title - onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(LOG_TAG, "$title - onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(LOG_TAG, "$title - onStop")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(LOG_TAG, "$title - onSaveInstanceState")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(LOG_TAG, "$title - onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(LOG_TAG, "$title - onDestroy")
+    }
+}
